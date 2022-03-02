7c7152bb082235e4684d9256483a5abef38e1958
Erdem Beştaş <84537386+erdembestas@users.noreply.github.com>
2022-03-02 22:57:58 +0300

    Update Main.java

--- taksimetre-algoritmasi/src/com/company/Main.java
+++ taksimetre-algoritmasi/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner val = new Scanner(System.in);
-	    double startVal=10.0, kmVal= 2.20, finalVal, distance;
+	double startVal=10.0, kmVal= 2.20, finalVal, distance;
         System.out.print("Add the distance :");
         distance = val.nextFloat();
         finalVal= startVal+(kmVal*distance);
