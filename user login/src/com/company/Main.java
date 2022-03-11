77eb69055a1099ca2dccab7991d1f8b670023b19
Erdem Beştaş <84537386+erdembestas@users.noreply.github.com>
2022-03-11 14:34:06 +0300

    Update Main.java

--- user login/src/com/company/Main.java
+++ user login/src/com/company/Main.java
@@ -3,7 +3,7 @@ import  java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	String userName, password;
+    String userName, password;
     Scanner inp= new Scanner(System.in);
 
     System.out.println("User name:");
